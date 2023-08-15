b98fbf9d7d918821c5891816bbe7d17995842b8d
T Mark <tmk1.tran@gmail.com>
2023-08-15 00:08:36 -0500

    added albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,3 +11,23 @@ function addToCollection( title, artist, yearPublished ) {
     collection.push(newAlbum);
     return newAlbum;
 }
+
+let album1 = addToCollection( 'Hybrid Theory', 'Linkin Park', 2000 );
+console.log( album1 );
+
+let album2 = addToCollection( 'Enter the Wu-Tang', 'Wu-Tang Clan', 1993 );
+console.log( album2 );
+
+let album3 = addToCollection( 'Dark Side of the Moon', 'Pink Floyd', 1973 );
+console.log( album3 );
+
+let album4 = addToCollection( 'Thriller', 'Michael Jackson', 1983 );
+console.log( album4 );
+
+let album5 = addToCollection( 'Legend: The Best of Bob Marley & The Wailers', 'Bob Marley', 1984 );
+console.log( album5 );
+
+let album6 = addToCollection( 'Scary Monsters and Nice Sprites', 'Skrillex', 2010 );
+console.log( album6 );
+
+console.log( collection );
