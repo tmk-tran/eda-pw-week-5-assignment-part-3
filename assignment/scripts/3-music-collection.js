1642e9e5781668cef3d1a1e2bf587623681aa181
T Mark <tmk1.tran@gmail.com>
2023-08-14 23:51:04 -0500

    created function addToCollection

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,3 +1,13 @@
 console.log('***** Music Collection *****')
 
 let collection = [];
+
+function addToCollection( title, artist, yearPublished ) {
+    const newAlbum = {
+        title: title,
+        artist: artist,
+        yearPublished: yearPublished
+    };
+    collection.push(newAlbum);
+    return newAlbum;
+}
